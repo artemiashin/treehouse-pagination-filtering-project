6773fbc6177728f1735f8df1c604d6275a7e7c04
artemiashin <rtem.iashin@gmail.com>
2023-01-17 12:39:57 +0400

    Comments added
    srcipt.js

--- js/script.js
+++ js/script.js
@@ -17,15 +17,30 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+
+// Declare a constant variable to store the number of items per page
 const num = 9;
 
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
 function showPage(list, page) {
+   // Determine the start and end index of the students to be displayed on the current page
    const start = (page*num) - num;
    const end = page*num;
+
+   // Get the ul element that will hold the student list
    const ul = document.querySelector('.student-list');
+
+   // Clear the ul element of any previous content
    ul.innerHTML = '';
+
+   // Loop through the list of students
    for (let i in list) {
+      // If the current index is within the range for the current page
       if (i>=start && i<end) {
+         // Create a string to hold the HTML for the current student
          let el = '';
          el += `<li class="student-item cf">
          <div class="student-details">
@@ -37,7 +52,8 @@ function showPage(list, page) {
            <span class="date">${list[i]['registered']['date']}</span>
          </div>
          </li>`;
-      ul.insertAdjacentHTML('beforeend', el);
+         // Append the HTML for the current student to the ul element
+         ul.insertAdjacentHTML('beforeend', el);
       }
    }
    
@@ -47,23 +63,33 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination (list) {
+   // Get the number of pages needed based on the list length and number of items per page
    const numPages = Math.floor(list.length/num + 1);
+   // Get the element to hold the pagination buttons
    const ul = document.querySelector('.link-list');
+   // Clear any existing buttons
    ul.innerHTML = '';
    let buttons = '';
-   for (let j = 1; j < numPages; j++) {
+   // Loop through and create the pagination buttons
+   for (let j = 1; j <= numPages; j++) {
       buttons += `<li><button type="button">${j}</button></li>`;
    }
+   // Add the buttons to the page
    ul.insertAdjacentHTML('beforeend', buttons);
+   // Set the first button as active
    const firstButton = document.querySelector('button');
    firstButton.className = 'active';
+   // Add an event listener to handle button clicks
    ul.addEventListener('click', (e) => {
       if (e.target.tagName === 'BUTTON') {
+         // Remove the active class from all buttons
          const buttons = document.querySelectorAll('button');
          for (let button of buttons) {
             button.className = '';
          }
+         // Add the active class to the clicked button
          e.target.className = 'active';
+         // Call the showPage function to update the displayed items
          showPage(list, e.target.textContent);
       }
    });
@@ -81,19 +107,27 @@ const searchField = document.querySelector('#search');
 const searchButton = document.querySelector('#search-button');
 
 function searcher (searchInput, list) {
+   // Initialize an empty array to hold the matching items
    let searchList = [];
-   for (let i in list) {
-     if (searchInput.value.length !== 0 && (list[i]['name']['first'].toLowerCase().includes(searchInput.value.toLowerCase()) || list[i]['name']['last'].toLowerCase().includes(searchInput.value.toLowerCase()))) {
-       searchList.push(list[i]);
-       showPage(searchList,1);
-       addPagination(searchList);
-     } else if (searchInput.value.length === 0){
-      showPage(data,1);
-      addPagination(data);
-     }
-   }
    
+   // Check if the search input is not empty
+   if (searchInput.value.length !== 0) {
+     // Loop through the list and check for matches
+     for (let i in list) {
+       if (list[i]['name']['first'].toLowerCase().includes(searchInput.value.toLowerCase()) || list[i]['name']['last'].toLowerCase().includes(searchInput.value.toLowerCase())) {
+         searchList.push(list[i]);
+       }
+     }
+     // Show the matching items and create pagination buttons
+     showPage(searchList,1);
+     addPagination(searchList);
+   } else {
+     // If the search input is empty, show the original list and create pagination buttons
+     showPage(data,1);
+     addPagination(data);
+   }
  }
+ 
 
  /* submit listener */
 searchButton.addEventListener('click', (event) => {
